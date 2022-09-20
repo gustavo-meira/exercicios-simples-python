93284fea636a5f80110a7a75c793f4b1ec0e98cf
Gustavo Meira <gmeira.antonio@gmail.com>
2022-09-20 11:41:58 -0300

    feat: implement exercise 3

---
+++ exercise3/README.md
@@ -0,0 +1,3 @@
+## Exercício 3
+
+O exercício trata sobre operadores matemáticos e ordens de precedência para as operações.
---
+++ exercise3/exercise3.py
@@ -0,0 +1,10 @@
+print("Exercício sobre operadores matemáticos")
+
+print(25 + 30 / 6)
+print(100 - 25 * 3 % 4)
+
+print("É verdade que 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+
+print("Quanto é 3 + 2?", 3 + 2)
+print("Quanto é 5 - 7?", 5 - 7)
